da4039523782864ea40890f9ffc1252fd1cf84a4
avishkaindunil <haavishkaindunil@gmail.com>
2025-03-17 22:00:46 +0530

    array created

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// #define LOG(x) std::cout << x << std::endl;
+
+using namespace std;
+
+int main() {
+    int arrOne[] = {1, 2, 3, 4, 5}; 
+
+    for (int i = 0; i <= 4; i++) {
+        cout << arrOne[i] << " " << endl;
+    }
+
+    return 0;
+}
